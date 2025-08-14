840bc45af14ed815a3d82281504c98b3ae0bd618
Kerim Hilmi Şahin <h.kerimsahin@outlook.com.tr>
2025-08-14 19:56:58 +0300

    #1 - Abstract ve Entity yapısı oluşturuldu

---
+++ src/BackendFirstStage.Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using BackendFirstStage.Domain.Entities.Seedwork;
+
+namespace BackendFirstStage.Domain.Entities;
+public class Product : BaseEntity
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public int StockQuantity { get; set; }
+    public string ImageUrl { get; set; }
+
+    public Product()
+    {
+        Name = string.Empty;
+        Description = string.Empty;
+        ImageUrl = string.Empty;
+    }
+}
---
+++ src/BackendFirstStage.Domain/Entities/Seedwork/BaseEntity.cs
@@ -0,0 +1,44 @@
+﻿namespace BackendFirstStage.Domain.Entities.Seedwork;
+
+public abstract class BaseEntity
+{
+    public int Id { get; set; }
+    public bool IsActive { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
+
+    //public int? CreatedBy { get; set; }
+    //public int? UpdatedBy { get; set; }
+    //public int? DeletedBy { get; set; }
+
+    public BaseEntity()
+    {
+        CreatedAt = DateTime.UtcNow;
+        IsActive = true;
+        IsDeleted = false;
+    }
+}
+
+public abstract class BaseEntity<T>
+{
+    public T Id { get; set; }
+    public bool IsActive { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
+
+    //public T CreatedBy { get; set; }
+    //public T? UpdatedBy { get; set; }
+    //public T? DeletedBy { get; set; }
+
+    public BaseEntity()
+    {
+        CreatedAt = DateTime.UtcNow;
+        IsActive = true;
+        IsDeleted = false;
+    }
+
+}
